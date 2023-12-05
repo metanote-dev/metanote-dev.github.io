55a2af2ee0e907841431e026591106a7f47bebe5
Metanote Team <metanote.team@gmail.com>
2023-12-05 18:49:12 +0800

    move analytics from template to publish config

--- publishconf.py
+++ publishconf.py
@@ -9,6 +9,15 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = "https://metanote-dev.github.io"
+ANALYTICS  = """<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-0E7KGQHGD6"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-0E7KGQHGD6');
+</script>"""
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = None
